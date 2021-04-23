f0d35c03f449f60cb7543ae77a69d3c7265f6a16
Guilherme Galaverna <guigalaverna@gmail.com>
2021-04-23 02:31:36 -0300

    :rocket: Formatando dados da API

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,28 +2,30 @@ import Head from 'next/head'
 
 import { GetStaticProps } from 'next'
 
-import c from '../services/api'
 import api from '../services/api'
-interface EpisodesProps {
-  episodes: Array<{
-    id: string,
-    title: string,
-    members: string,
-    published_at: string,
-    thumbnail: string,
-    description: string,
-    file: {
-      url: string,
-      type: 'audio/x-m4a',
-      duration: number
-    }
-  }>
+
+import { format, parseISO } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+import { convertDurationToTimeString } from '../utils/convertDuration'
+interface Episode {
+  id: string;
+  title: string;
+  thumbnail: string;
+  members: string;
+  publishedAt: string;
+  duration: number;
+  durationAsString: string;
+  description: string;
+  url: string;
+}
+
+interface HomeProps {
+  episode: Episode[]
 }
 
 
-export default function Home(props: EpisodesProps) {
-  console.log(props.episodes)
-
+export default function Home(props: HomeProps) {
+  console.log(props)
   return (
     <>
       <Head>
@@ -38,12 +40,31 @@ export default function Home(props: EpisodesProps) {
 
 
 export const getStaticProps: GetStaticProps = async () => {
-  const response = await api.get('/episodes', { params: { _limit: 12, _order: 'desc', _sort: 'published_at' }})
-  const data = await response.data
+  const { data } = await api.get('/episodes', {
+    params: {
+      _limit: 12,
+      _sort: 'published_at',
+      _order: 'desc'
+    }
+  })
+
+  const episodes = data.map(episode => {
+    return {
+      id: episode.id,
+      title: episode.title,
+      thumbnail: episode.thumbnail,
+      members: episode.members,
+      publishedAt: format(parseISO(episode.published_at), 'd MMM yy', { locale: ptBR}),
+      duration: Number(episode.file.duration),
+      durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
+      description: episode.description,
+      url: episode.file.url
+    }
+  })
 
   return {
     props: {
-      episodes: data
+      episodes
     },
     revalidate: 60 * 60 * 8
   }
