e86a6f8f1fdf7a8592d4b5dd567267b7ea12581d
Guilherme Galaverna <guigalaverna@gmail.com>
2021-04-23 06:50:02 -0300

    :rocket: Play/Pause adicionado

--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -1,33 +1,43 @@
-import { createContext, useState } from 'react'
+import { createContext, useState } from "react";
 interface Episode {
-  title: string
-  members: string
-  thumbnail: string
-  duration: number
-  url: string
+  title: string;
+  members: string;
+  thumbnail: string;
+  duration: number;
+  url: string;
 }
 
 interface PlayerContextData {
   episodeList: Episode[];
-  currentEpisodeIndex: number
+  currentEpisodeIndex: number;
+  isPlaying: boolean;
 
-  play: (episode) => void
+  play: (episode) => void;
+  tooglePlay: () => void;
 }
 
-export const PlayerContext = createContext({} as PlayerContextData)
+export const PlayerContext = createContext({} as PlayerContextData);
 
 export default function PlayerProvider({ children }) {
-  const [episodeList, setEpisodeList] = useState([])
-  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0)
+  const [episodeList, setEpisodeList] = useState([]);
+  const [currentEpisodeIndex, setCurrentEpisodeIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(false);
 
   function play(episode) {
-    setEpisodeList([episode])
-    setCurrentEpisodeIndex(0)
+    setEpisodeList([episode]);
+    setIsPlaying(true);
+    setCurrentEpisodeIndex(0);
+  }
+
+  function tooglePlay() {
+    setIsPlaying(!isPlaying);
   }
 
   return (
-    <PlayerContext.Provider value={{ currentEpisodeIndex, episodeList, play }}>
-      { children }
+    <PlayerContext.Provider
+      value={{ currentEpisodeIndex, episodeList, play, isPlaying, tooglePlay }}
+    >
+      {children}
     </PlayerContext.Provider>
-  )
+  );
 }
